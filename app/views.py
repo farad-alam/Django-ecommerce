1f52f5e359faa81ebdb2ec3ce53e8e47bce76e34
faradalam <farad.alam1997@gmail.com>
2023-09-01 22:40:13 +0600

    increse item, decrese item, remove item from CART

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 from . models import *
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from . forms import UserRegistrationForm, CustomerForm
 from django.contrib import messages
 from django.views import View
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 def home(request):
  dels_of_the_day_products = Products.objects.all()
  cosmetics_obj = Categories.objects.get(name='Cosmetics')
@@ -29,7 +31,7 @@ def categories_page(request,id):
  products = Products.objects.filter(categories=id)
  categories = Categories.objects.all()
  display_categori = Categories.objects.get(id=id)
- context = {'categories':categories,'products':products,'display_categori':display_categori}
+ context = { 'categories':categories,'products':products,'display_categori':display_categori}
  return render(request, 'app/categories_page.html',context)
 
 def filter_price(request, data, id):
@@ -91,7 +93,7 @@ class CustomerProfileView(View):
    return render(request,'app/profile.html',context)
 
 def address(request):
- addresses = Customer.objects.all()
+ addresses = Customer.objects.filter(user=request.user)
  context = {'addresses':addresses,'active':'btn-primary'}
  return render(request, 'app/address.html',context)
 
@@ -107,14 +109,97 @@ def add_to_cart(request,id):
 def show_cart(request):
  user = request.user
  products = Cart.objects.filter(user=user)
+ total_product_count = products.count()
  total_ammount = 0.00
- shipping_charge = 70.00
- for product in products:
-  temp_ammount = product.product.discounted_price * product.quantity
-  total_ammount += temp_ammount
- total_payable = total_ammount+shipping_charge
- context = {'products':products,'total_payable':total_payable,'shipping_charge':shipping_charge,'total_ammount':total_ammount}
+ total_payable = 0.00
+ shipping_charge = 00.00
+ if products:
+  shipping_charge = 70.00
+  for product in products:
+    temp_ammount = product.product.discounted_price * product.quantity
+    total_ammount += temp_ammount
+  total_payable = total_ammount+shipping_charge
+ context = {'products':products,
+            'total_payable':total_payable,
+            'shipping_charge':shipping_charge,
+            'total_ammount':total_ammount,
+            'total_product_count':total_product_count
+            }
  return render(request, 'app/addtocart.html',context)
+
+@csrf_exempt
+def increase_cart(request):
+ if request.method == 'POST':
+  # product_id = request.GET.get('value')
+  # print(request.body)
+  # print(request.POST)
+  data = request.body
+  # print(data)
+  data = json.loads(data)
+  print(data)
+  # product_id = request.GET['productId']
+  # product_id = data.get('productId')
+  product_id = data['productId']
+  number = data['number']
+  print(product_id)
+  product_ins= Products.objects.get(id=product_id)
+  product_from_cart = Cart.objects.filter(product=product_ins).filter(user=request.user)
+  # print(product_from_cart) 
+  # product_from_cart.quantity =+ 1
+  for catrs in product_from_cart:
+   if number == 2:
+    catrs.quantity += 1
+    catrs.save()
+   if number == 1:
+    if catrs.quantity > 1:
+     catrs.quantity -= 1
+     catrs.save()
+   if number == 0:
+    catrs.delete()
+    user_updated_product = Cart.objects.get(user=request.user)
+   print(catrs.quantity)
+   product_quantity = catrs.quantity
+   targeted_product = Products.objects.get(id=product_id)
+   product_price = catrs.quantity * targeted_product.discounted_price
+   
+  # print(product_from_cart.quantity)
+  # product_from_cart.save()
+
+  total_ammount = 0.0
+  total_payable = 0.00
+  shipping_charge = 70.00
+  products = Cart.objects.filter(user=request.user)
+  for product in products:
+    temp_ammount = product.product.discounted_price * product.quantity
+    total_ammount += temp_ammount
+  total_payable = total_ammount+shipping_charge
+  data = {
+   'product_quantity':product_quantity,
+   'product_price':product_price,
+   'total_ammount':total_ammount,
+   'total_payable':total_payable,
+   'shipping_charge':shipping_charge
+
+  }
+  # print(data)
+ return JsonResponse(data)
+
+def remove_item(request,id):
+#  product_ins = Products.objects.get(id=id)
+ Cart.objects.filter(id=id).filter(user=request.user).delete()
+ products = Cart.objects.filter(user=request.user)
+ total_ammount = 0.00
+ total_payable = 0.00
+ shipping_charge = 00.00
+ if products:
+  shipping_charge = 70.00
+  for product in products:
+    temp_ammount = product.product.discounted_price * product.quantity
+    total_ammount += temp_ammount
+  total_payable = total_ammount+shipping_charge
+ context = {'products':products,'total_payable':total_payable,'shipping_charge':shipping_charge,'total_ammount':total_ammount}
+ return redirect('cart')
+#  return render(request,'app/addtocart.html',context)
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
